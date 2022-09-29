fc13b751b241969b5bb037f1eb6a3593ac06d434
MattGueler <mateus.gueler@gmail.com>
2022-09-29 20:50:36 -0300

    feat: Create a route to return user infos from token

--- src/Routers/UserRouter.ts
+++ src/Routers/UserRouter.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
-import { LoginUser, RegisterUser } from "../Controller/UserController";
+import {
+  GetUserInfos,
+  LoginUser,
+  RegisterUser,
+} from "../Controller/UserController";
 import { validateSchema } from "../Middlewares/ValidateSchemaMiddleware";
 import registerSchema from "../Schemas/RegisterSchema";
 import loginSchema from "../Schemas/LoginSchema";
+import { validatingToken } from "../Middlewares/ValidateToken";
 
 const userRouter = Router();
 
 userRouter.post("/sign-up", validateSchema(registerSchema), RegisterUser);
 userRouter.post("/sign-in", validateSchema(loginSchema), LoginUser);
+userRouter.get("/user/me", validatingToken, GetUserInfos);
 
 export default userRouter;
