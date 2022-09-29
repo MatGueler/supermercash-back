c9625a02da8ac8f11c3ea3e0a334e8f42ecaf86f
MattGueler <mateus.gueler@gmail.com>
2022-09-28 22:08:40 -0300

    feat: Create functions of controller, service and repository to register

--- src/Service/UserService.ts
+++ src/Service/UserService.ts
@@ -1,5 +1,11 @@
 import * as loginRepository from "../Repository/UserRepository";
 
+export async function registerUser() {
+  // const token = generateToken();
+  await loginRepository.loginUser();
+  return "oi";
+}
+
 export async function loginUser() {
   // const token = generateToken();
   await loginRepository.loginUser();
