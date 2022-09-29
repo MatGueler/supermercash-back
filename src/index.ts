6a8cf6565d8275d8464b2f7fdec53de221ffb48e
MattGueler <mateus.gueler@gmail.com>
2022-09-28 21:13:51 -0300

    chore: Import errorHandlerMiddleware in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
 import "express-async-errors";
-// import errorHandler from "./Middlewares/errorHandler";
+import { errorHandlerMiddleware } from "./Middlewares/errorHandler";
 // import routes from "./Routers/Index";
 
 const server = express();
 server.use(express.json());
 server.use(cors());
 // server.use(routes);
-// server.use(errorHandler);
+server.use(errorHandlerMiddleware);
 
 export default server;
