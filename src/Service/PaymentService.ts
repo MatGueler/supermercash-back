0aada5530b654c3d7844b3d9ef2260416fd3deab
MattGueler <mateus.gueler@gmail.com>
2022-10-05 15:26:06 -0300

    chore: Return error 422 if cart is empty

--- src/Service/PaymentService.ts
+++ src/Service/PaymentService.ts
@@ -2,11 +2,19 @@
 import * as paymentRepository from "../Repository/PaymentRepository";
 import { getUserById } from "../Repository/UserRepository";
 import { IPayment } from "../Types/PaymentType";
+import { conflictError, wrongSchemaError } from "../Utils/ErrorUtils";
 import { verifyPassword } from "./UserService";
 
 export async function makePayment(body: IPayment, userId: number) {
   const user = await getUserById(userId);
+  verifyCartIsEmpy(body.quantifyProducts);
   await verifyPassword(body.password, user.password);
   await paymentRepository.createPayment(body, userId);
   await paymentRepository.deletePurchases(userId);
+}
+
+function verifyCartIsEmpy(quantifyProducts: number) {
+  if (quantifyProducts === 0) {
+    throw wrongSchemaError("Cart can't be empty");
+  }
 }
