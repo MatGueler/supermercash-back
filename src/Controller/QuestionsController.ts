be02f2a0bec52605895dcb1e77dbfcb397dd4fc4
MattGueler <mateus.gueler@gmail.com>
2022-10-13 14:52:34 -0300

    fix:
    Try find get question function error

--- src/Controller/QuestionsController.ts
+++ src/Controller/QuestionsController.ts
@@ -2,6 +2,6 @@ import * as questionsService from "../Service/QuestionsServices";
 import { Request, Response } from "express";
 
 export async function GetAllQuestionsWithAnswers(req: Request, res: Response) {
-  const questions = await questionsService.getAllQuestions();
-  res.status(200).send('questions');
+  // const questions = await questionsService.getAllQuestions();
+  res.status(200).send("questions");
 }
